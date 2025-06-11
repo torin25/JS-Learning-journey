438e01907654347c29968d1187449e2c0afed303
tejasm <tejasmcodes@gmail.com>
2025-06-11 11:22:36 +0530

    Update:finalised basic calculator with input validation and improved UI

---
+++ calculator-challenge/README.md
@@ -0,0 +1,16 @@
+# Basic Calculator
+
+A simple calculator web app built with HTML, CSS, and JavaScript.
+
+## Features
+
+- Takes two numeric inputs
+- Performs:
+  - Addition
+  - Subtraction
+  - Multiplication
+  - Division
+- Displays result below the buttons
+- Shows a warning message if inputs are missing
+
+![app screenshot](calci.png)
--- calculator-challenge/calci.js
+++ calculator-challenge/calci.js
@@ -1,28 +1,41 @@
-let num1=2
-let num2=3
-document.getElementById("num1-el").textContent=num1
-document.getElementById("num2-el").textContent=num2
-let ans= document.getElementById("result")
+function values(){
+    let inputOne=document.getElementById("num1-el").value
+    let inputTwo=document.getElementById("num2-el").value
+    let ans= document.getElementById("result")
+    if (inputOne==="" || inputTwo===""){
+        ans.textContent="Result: Please enter both the numbers."
+        return
+    }
+
+    const num1=Number(inputOne)
+    const num2=Number(inputTwo)
+    return {num1, num2, ans}
+}
+
 
 function add(){
+    const {num1, num2, ans}=values()
     let sum=num1+num2
     ans.textContent="Result: "+sum
     console.log(sum)
 }
 
 function sub(){
+    const {num1, num2, ans}=values()
     let diff=num1-num2
     ans.textContent="Result: "+diff
     console.log(diff)
 }
 
 function div(){
+    const {num1, num2, ans}=values()
     let quotient=num1/num2
     ans.textContent="Result: "+quotient
     console.log(div)
 }
 
 function mul(){
+    const {num1, num2, ans}=values()
     let product=num1*num2
     ans.textContent="Result: "+product
     console.log(mul)
